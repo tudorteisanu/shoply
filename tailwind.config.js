65984949ace8718ecf612db6b5ffac2155553127
Tudor Teisanu <tudor.teisanu@powerit.dev>
2022-09-09 19:51:27 +0300

    added header and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          900: '#211F1C',
+          800: '#484543',
+          700: '#393D45',
+          600: '#616467',
+          500: '#ACACAC',
+          300: '#F8F8F8',
+          200: '#FEFDFB'
+        },
+      }
+    },
   },
   plugins: [],
 }
