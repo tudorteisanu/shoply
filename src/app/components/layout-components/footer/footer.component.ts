1204111fdb28619d50cf2077604fc4d568996dbe
Tudor Teisanu <tudor.teisanu@powerit.dev>
2022-10-29 17:03:45 +0300

    refactor semantic tags

--- src/app/components/layout-components/footer/footer.component.ts
+++ src/app/components/layout-components/footer/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
+  selector: 'Footer',
   templateUrl: './footer.component.html',
 })
 export class FooterComponent implements OnInit {
